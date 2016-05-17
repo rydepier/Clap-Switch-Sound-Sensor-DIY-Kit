2106739cfcbacc5646f10e51aeb3e32fc4dd6fd5
Chris Rouse <christopher.rouse@talktalk.net>
2016-05-17 22:20:48 +0100

    Add files via upload

---
+++ sketch_clap_switch_basic.ino
@@ -0,0 +1,42 @@
+/***********************************************************
+Clap Switch Simple Control
+by
+Chris Rouse
+May 2016
+
+Connections:
+
+***********************************************************/
+const byte ledPin = 13;
+boolean trigger = false; // used to show clap output ON
+int triggerLevel = 100; // change this if background noise is high
+int outputReading; // voltage output from Clap Switch
+
+/*********************************************************/
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW); // turn LED OFF
+
+}
+/*********************************************************/
+void loop() {
+  checkSwitch(); // see if the switch has been triggered
+  if(trigger){
+    digitalWrite(ledPin, HIGH);
+  }
+  else{
+    digitalWriteledPin, LOW);
+  }
+}
+/*********************************************************/
+void checkSwitch() {
+  outputReading = analogRead(14);
+  if(outputReading > background){
+    trigger = false
+  }
+  else{
+    trigger = true;
+  }
+}
+/*********************************************************/
